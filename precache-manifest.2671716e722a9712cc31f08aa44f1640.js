51fe0dad8b1dd680c95d8b3935933edb93500851
Swetha Tanuku (RBEI/BSW5) <swt6kor@bosch.com>
2020-04-29 20:47:57 +0530

    Updates

---
+++ precache-manifest.2671716e722a9712cc31f08aa44f1640.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ac9f669d0cf510d11d523d110f6001",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "cca7b32fe6f070c9a051",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5b52a7ae8ad4a3a3b802",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cca7b32fe6f070c9a051",
+    "url": "/contactmanager/static/js/2.5aac408d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.5aac408d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b52a7ae8ad4a3a3b802",
+    "url": "/contactmanager/static/js/main.f45ca788.chunk.js"
+  },
+  {
+    "revision": "98d49c782551fbed3aca",
+    "url": "/contactmanager/static/js/runtime-main.53501af3.js"
+  }
+]);
